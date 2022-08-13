d04e5ba60e2a6ecfc03015e1c806dfa52b500466
Rob8624 <robert_melen@yahoo.co.uk>
2022-08-13 01:36:33 +0100

    Initial commit

---
+++ NewSite/main/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from PIL import Image
+
+# Create your models here.
+
+class Media(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Media"
+
+    timestamp = models.DateTimeField()
+    image = models.ImageField(upload_to="media")
+    url = models.URLField()
+
---
+++ NewSite/main/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+from . models import Media
+
+
+# Create your views here.
+
+
+class HomeView(TemplateView):
+
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class AboutView(TemplateView):
+
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class ContactView(TemplateView):
+
+    template_name = "main/contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class MediaView(ListView):
+
+    template_name = "main/media.html"
+    model = Media
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
