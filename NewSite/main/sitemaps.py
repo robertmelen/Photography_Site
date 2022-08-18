0be8361311f6052be4198e99a753a99392dd3c49
Rob8624 <robert_melen@yahoo.co.uk>
2022-08-19 00:41:33 +0100

    Html theme added

---
+++ NewSite/main/sitemaps.py
@@ -0,0 +1,19 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+class StaticViewSitemap(sitemaps.Sitemap):
+
+    priority = 0.5
+    changefreq = "daily"
+
+    def items(self):
+
+        return  [
+            'main:home',
+            'main:about',
+            'main:contact',
+            'main:media'
+                    ]
+
+    def location(self, item):
+        return reverse(item)
