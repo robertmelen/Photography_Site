1ff1491ef4910a8a93ca78ff9efd5dad48c753ed
Rob8624 <robert_melen@yahoo.co.uk>
2022-10-21 14:04:59 +0100

    comments working, popular posts context processor added, fixed a few things

--- NewSite/main/custom_context_processor.py
+++ NewSite/main/custom_context_processor.py
@@ -1,13 +1,20 @@
-from .models import Profile, Category, BlogPost
-
+from .models import Profile, Category, BlogPost, Post_Category
+from django.contrib.auth.decorators import user_passes_test
 
 def user_info(request):
-   user = request.user
-   current_user = Profile.objects.get(user=user)
-   return {'user': current_user }
+
+    user = request.user
+    if user.is_authenticated:
+        current_user = Profile.objects.get(user=user)
+        return {'user': current_user }
+    else:
+        return {'user': Profile.objects.get(user='1')}
 
 def post_info(request):
   popular_posts = BlogPost.objects.filter(status='published').order_by('-created')[0:3]
   return {'popular_posts': popular_posts}
 
+def category_info(request):
+  categories = Post_Category.objects.all()[0:2]
+  return {'categories': categories}
 
