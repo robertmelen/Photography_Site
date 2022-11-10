d0e471a1cff80e18a16c4ecf9940b43c3e55c9f2
rob8624 <86303975+rob8624@users.noreply.github.com>
2022-11-10 00:19:37 +0000

    Add files via upload

---
+++ NewSite/NewSite/urls.py
@@ -0,0 +1,45 @@
+"""NewSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from main.sitemaps import StaticViewSitemap
+from django.contrib.sitemaps.views import sitemap
+
+sitemaps = {
+
+    'sitemap': StaticViewSitemap
+
+}
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace="main")),
+    re_path(r'^maintenance-mode/', include('maintenance_mode.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+     name='django.contrib.sitemaps.views.sitemap'), #this is straight from docs
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('summernote/', include('django_summernote.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
