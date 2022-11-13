91ad2d6bda56b289bfdf266fd039994bc82220a4
Rob8624 <robert_melen@yahoo.co.uk>
2022-11-13 23:57:56 +0000

    added migrations

--- NewSite/main/custom_context_processor.py
+++ NewSite/main/custom_context_processor.py
@@ -1,7 +1,9 @@
 from .models import Profile, Category, BlogPost, Post_Category, Tag, Settings
 from django.contrib.auth.decorators import user_passes_test
 
-
+def user_info(request):
+   current_user = Profile.objects.get(user='1')
+   return {'user': current_user }
 
 def post_info(request):
   popular_posts = BlogPost.objects.filter(status='published').order_by('-created')[0:3]
