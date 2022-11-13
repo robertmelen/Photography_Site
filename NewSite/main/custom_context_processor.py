1e10e27a57abf2855596200c8201e47e8fd92974
Rob8624 <robert_melen@yahoo.co.uk>
2022-11-13 22:05:22 +0000

    trying to fix user removed user check

--- NewSite/main/custom_context_processor.py
+++ NewSite/main/custom_context_processor.py
@@ -2,12 +2,8 @@ from .models import Profile, Category, BlogPost, Post_Category, Tag, Settings
 from django.contrib.auth.decorators import user_passes_test
 
 def user_info(request):
-    user = request.user
-    if user.is_authenticated:
-        current_user = Profile.objects.get(user=user)
-        return {'user': current_user }
-    else:
-        return {'user': Profile.objects.get(user='1')}
+   current_user = Profile.objects.get(user='1')
+   return {'user': current_user }
 
 def post_info(request):
   popular_posts = BlogPost.objects.filter(status='published').order_by('-created')[0:3]
